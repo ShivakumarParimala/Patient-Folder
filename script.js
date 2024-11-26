796e72b4ac2832a09baeca5bf383c4a3d4aa8024
shiva <shivakumarparimala1@gmail.com>
2024-11-26 15:23:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+[
+    {
+        "name": "Jessica Taylor",
+        "bloodPressure": [
+            { "year": 2020, "value": 120 },
+            { "year": 2021, "value": 122 },
+            { "year": 2022, "value": 118 },
+            { "year": 2023, "value": 125 }
+        ]
+    },
+    {
+        "name": "John Doe",
+        "bloodPressure": [
+            { "year": 2020, "value": 130 },
+            { "year": 2021, "value": 128 },
+            { "year": 2022, "value": 135 }
+        ]
+    }
+]
